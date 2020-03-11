5f66bd876f7d3771e6d2d900c4bb229ca9fb1d07
sergio-hernandez-castillo <a01025210@itesm.mx>
2020-03-11 15:40:13 -0600

    Se crearon nuevos archivos de código para empezar.

---
+++ sources/ArbolAVL.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "Aqui programaremos el Árbol AVL." << endl;
+    cout << "Cuando ya este hecho se va a implementar al 'Main.cpp'" << endl;
+
+    return 0;
+}//Close main 
---
+++ sources/Main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "Aqui se implementaran los dos árboles en 2 clases." << endl;
+
+    return 0;
+}//Close main 
